44d95639a36c63da846c9db712aaf71ab08d329d
VATSALKUMAR HARSHADBHAI PAVAR <vatsalkumar018@gmail.com>
2026-09-18 04:59:30 +0530

    small changes in code

--- practice/code1.c
+++ practice/code1.c
@@ -1,19 +1,7 @@
 #include<stdio.h>
 
-int sumofnumbersdigit(int number);
+int reverse_number(int number);
 
 int main(){
-    
-    int number;
-    
-    printf("enter sny numeber:");
-    
-    scanf("%d",&number);
-    
-    printf("our sum of ditin is :%d", sumofnumbersdigit(number));
-
-}
-
-int sumofnumbersdigit(int number){
     
 }
